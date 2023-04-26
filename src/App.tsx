527337fae410c615aaf4743247d58b763263ca50
irishcarbomb777 <danielclannan@gmail.com>
2023-04-26 16:38:06 -0400

    Clean template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+export const App = () =>  {
+    return (
+        <>
+            <main className={styles.main}>
+                <img className={styles.logo} src="thinair.svg"/>
+            </main>
+        </>
+    )
+}
+
